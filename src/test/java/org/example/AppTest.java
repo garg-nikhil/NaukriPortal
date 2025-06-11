5a85cf190392a4f4c5550b037ec43ada2a4a5cb5
Nikhil Garg <nikhilgarg855@gmail.com>
2025-06-11 16:55:33 +0530

    fix

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,6 +3,8 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,6 +12,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 
 
@@ -21,7 +28,7 @@ public class AppTest {
     public void updateNaukri(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // for Chrome 109+
+      //  options.addArguments("--headless=new"); // for Chrome 109+
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
@@ -32,20 +39,22 @@ public class AppTest {
         driver.manage().window().maximize();
         driver.get("https://login.naukri.com/nLogin/Login.php");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //        String label = "I'm Feeling Lucky";
-        //        String x = "//*[@aria-label="+label+"][1]";
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(screenshot.toPath(), Paths.get("screenshot.png"), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         driver.findElement(By.xpath("//*[@id='usernameField']")).sendKeys("nikhilgarg855@gmail.com");
         driver.findElement(By.xpath("//*[@id='passwordField']")).sendKeys("Sons@1234");
         driver.findElement(By.xpath("//button[text()='Login']")).click();
 
         driver.findElement(By.xpath("//*[text()='View']")).click();
-        driver
-                .findElement(
-                        By.xpath(
-                                "//*[text()='Resume headline']/following-sibling::span[text()='editOneTheme']"))
-                .click();
+        driver.findElement(By.xpath("//*[text()='Resume headline']/following-sibling::span[text()='editOneTheme']")).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[text()='Save']")));
         driver.findElement(By.xpath("//*[text()='Save']")).click();
+        driver.quit();
     }
 }
